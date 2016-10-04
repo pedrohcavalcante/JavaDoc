61757645bc2a04adfe234cba1fa0952078c12ef1
pedrinho.sjs <pedrinho.sjs@A305-03L.imd.local>
2016-10-04 09:34:59 -0300

    Javadoc

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,4 +1,10 @@
-
+/**
+ * Classe para cadastrar os usuários
+ * @author Pedro Henrique
+ * @version 1.0 - 04/10/2016
+ * 
+ *
+ */
 public class Aplicacao {
 
 	public static void main(String[] args) {
