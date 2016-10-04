046a3b9937f1d75541e9f094ba47335aae76ecd1
pedrinho.sjs <pedrinho.sjs@A305-03L.imd.local>
2016-10-04 09:31:07 -0300

    Definição de documentação, inserindo comentários sobre as classes e seus atributos.

--- src/Contato.java
+++ src/Contato.java
@@ -1,13 +1,28 @@
-
+/**
+ * Classe para guardar contatos
+ * @author Pedro Henrique
+ * @version 1.0 - 04/10/2016
+ * 
+ */
 public class Contato {
-	
+	/**
+	 * nome do contato
+	 */
 	private String nome;
-	
+	/**
+	 * email do contato
+	 */
 	private String email;
-	
+	/**
+	 * endereço do contato
+	 */
 	private Endereco endereco;
 
-	
+	/**
+	 * Método construtor
+	 * @param nome
+	 * @param email
+	 */
 	public Contato(String nome, String email) {
 
 		this.nome = nome;
@@ -15,19 +30,29 @@ public class Contato {
 
 	}
 
-
+	/**
+	 * Método para definir o endereço
+	 * @param rua
+	 * @param numero
+	 */
 	public void definirEndereco(String rua, int numero) {
 		endereco.setRua(rua);
 		endereco.setNumero(numero);
 
 	}
 
-	
+	/**
+	 * 
+	 * @return retorna o nome do contato
+	 */
 	public String getNome() {
 		return nome;
 	}
 
-	
+	/**
+	 * 
+	 * @return retorna o email do contato
+	 */
 	public String getEmail() {
 		return email;
 
